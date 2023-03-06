03b2539ee7ffd528a3bded8f2745900dad56851a
Felipe1501 <felipebsantos1501@gmail.com>
2023-03-06 16:27:50 -0300

    adicionando elemento a uma lista no python

---
+++ add_item_list/app.py
@@ -0,0 +1,20 @@
+list = [10, 20, 30, 40, 50]
+
+print(list)
+
+list.append(60)
+
+print(list)
+
+people = ['Felipe', 18, True]
+
+print(people)
+
+people.append([1, 2, 3])
+
+print(people)
+
+print(people[0])
+
+for i in people:
+    print(i)
