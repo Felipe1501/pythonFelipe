448f8c737e993a394b2a8e0b286c87eabdee09ca
Felipe1501 <felipebsantos1501@gmail.com>
2023-03-06 16:36:11 -0300

    converter lista para string com python

---
+++ list_for_string/app.py
@@ -0,0 +1,14 @@
+myList = ["Python", "é", "bão", "demais!!!"]
+
+print(myList)
+
+myString = " ".join(myList)
+
+print(myString)
+
+print(type(myString))
+print(type(myList))
+
+list = ["15", "01"]
+
+print("-".join(list))
