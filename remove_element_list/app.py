5593a289a4eee0920144500aa33f376c857f8d02
Felipe1501 <felipebsantos1501@gmail.com>
2023-03-06 16:51:40 -0300

    remover elemento de uma lista com python

---
+++ remove_element_list/app.py
@@ -0,0 +1,15 @@
+l = [100, 200, 300, 400, 500, 600]
+
+print(l)
+
+# pop sem argumento, remove o último elemento
+l.pop()
+
+print(l)
+
+# indice especifico remove elemento determinado
+print(l[2])
+
+l.pop(2)
+
+print(l)
